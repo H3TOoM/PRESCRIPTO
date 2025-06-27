f44704c34ac7f7b6957da1ab9222291abb9b1493
Hatim <altorbed7@gmail.com>
2025-06-27 15:46:22 +0300

    update specialty section

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -7,7 +7,7 @@ const SpecialityMenu = () => {
   const { t } = useTranslation();
   return (
     <div id="speciality" className="flex flex-col items-center gap-4 py-16 text-gray-800 m-15">
-      <h1 className="text-3xl font-medium">{t("speciality_title")}</h1>
+      <h1 className="text-4xl font-medium">{t("speciality_title")}</h1>
       <p className="sm:w-1/3 text-center text-sm">
         {t("speciality_subtitle")}
       </p>
@@ -15,7 +15,7 @@ const SpecialityMenu = () => {
         {specialityData.map((item , index) => (
             <Link key={index} to={`/doctors/${item.speciality}`} onClick={() => scrollTo(0,0)} className=" flex flex-col items-center text-xs cursor-pointer flex-shirnk-0 hover:translate-y-[-10px] transition-all duration-500">
                 <item.image  alt="" className="w-16 sm:w-24 mb-2 "/>
-                <p className="text-sm text-center special-desc">{t(`speciality_${item.speciality}`)}</p>
+                <p className="text-lg text-center special-desc">{t(`speciality_${item.speciality}`)}</p>
             </Link>
         ))}
       </div>
